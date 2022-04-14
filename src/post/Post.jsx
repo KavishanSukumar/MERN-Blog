ccba60199ea59a8e8435ee4eeb250687447c4ff2
KavishanSukumar <kavishansukumar@gmail.com>
2022-04-14 21:21:34 +0530

    Completing post

---
+++ src/post/Post.jsx
@@ -0,0 +1,39 @@
+import './post.css';
+
+export default function post() {
+  return (
+    <div className='post'>
+        <img className='postImg' src="https://i.picsum.photos/id/1023/3955/2094.jpg?hmac=AW_7mARdoPWuI7sr6SG8t-2fScyyewuNscwMWtQRawU" alt="" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+                Lorem, ipsum dolor sit amet 
+            </span>
+            <hr />
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+            Lorem ipsum dolor sit amet consectetur,
+             adipisicing elit. Mollitia tenetur neque vitae
+              debitis corrupti. Quo inventore veniam minus, 
+              similique porro esse nisi iste accusamus est tempore, 
+              quasi possimus sequi hic? Lorem ipsum dolor sit amet 
+              consectetur adipisicing elit. Non, delectus veniam quis 
+              voluptates ex, mollitia consequuntur laborum aperiam 
+              explicabo, repudiandae similique nobis laboriosam facilis 
+              in saepe? Quibusdam obcaecati officiis optio voluptatum 
+              sit necessitatibus exercitationem ad recusandae facere. 
+              Non maxime vero recusandae aliquid quasi excepturi ipsa 
+              consectetur qui? Aperiam, quae eaque voluptas officiis 
+              dolores quos, itaque dolor ab, quis doloribus ut. Tempore 
+              quae quis pariatur neque at consequatur, magnam atque 
+              laudantium officiis sunt corporis odio rem excepturi
+               maxime hic veniam? Rem sunt similique illum repellat
+                amet vitae. Ipsum inventore autem possimus unde excepturi pariatur quam sed nemo voluptas quaerat molestiae nam adipisci provident tempora animi quod officiis, odio sint consequatur ad atque facilis? Consectetur facere, enim incidunt odit magnam sint optio consequuntur perspiciatis repellat asperiores, vitae earum eligendi at hic delectus temporibus veniam facilis commodi expedita magni ducimus? Deserunt a molestiae sunt delectus impedit iusto, tenetur eveniet cum laudantium esse, voluptates facilis illum aperiam repellendus error expedita sapiente numquam, nemo sit. Odio eveniet aspernatur enim? Minus obcaecati rem hic perspiciatis, possimus aut minima at magni nesciunt unde esse doloremque eum exercitationem reiciendis odit nobis dolorum, maxime non. Similique et, illo molestias modi maiores atque velit inventore, sint eligendi sit magni distinctio?
+        </p>
+    </div>
+  )
+}
